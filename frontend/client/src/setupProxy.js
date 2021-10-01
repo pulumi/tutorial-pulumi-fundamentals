8f045b0a328e1485bfe6f2d90d42bb72ba87e274
Laura Santamaria <laura@nimbinatus.com>
2021-10-01 15:14:20 -0500

    fix(proxy): fix proxy use for cloud-based systems

--- frontend/client/src/setupProxy.js
+++ frontend/client/src/setupProxy.js
@@ -1,12 +1,13 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 const host = process.env.HTTP_PROXY;
+const protocol = process.env.PROTOCOL;
 
 module.exports = function(app) {
   app.use(
     '/api',
     createProxyMiddleware({
-      target: 'http://' + `${host}`,
+      target: `${protocol}${host}`,
       changeOrigin: true,
     })
   );
